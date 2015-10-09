9b421f65dd14b1a782768ac03e590d07d334168f
Matt Aschmann <matt.aschmann@gmail.com>
2015-10-09 11:41:29 -0600

    Initial commit

---
+++ all-output.js
@@ -0,0 +1,67 @@
+// @Matt DOCS
+
+var parsedPlayers = [];
+
+var $nextButton = $('.pagingnav .last a');
+
+scrapePlayers();
+$nextButton.click();
+
+var interval = setInterval(function() {
+  var exitEarly = scrapePlayers();
+
+  $nextButton = $('.pagingnav .last a');
+  if (exitEarly || $nextButton === null) {
+    clearInterval(interval);
+    interval = null;
+
+    copy(parsedPlayers.join('\n'));
+    alert('Finished!');
+  } else {
+    $nextButton.click();
+  }
+}, 1000);
+
+function scrapePlayers() {
+  var players = document.querySelectorAll('.players tbody tr');
+
+  for (var i = 0; i < players.length; i++) {
+    var player = players[i].querySelectorAll('td');
+
+    var owner = '';
+    if (player[3].querySelector('a')) {
+      owner = player[3].querySelector('a').innerText;
+    } else {
+      owner = player[3].querySelector('div').innerText;
+    }
+
+    var parsedPlayer = [
+      player[1].querySelector('.name').innerText, // player name
+      owner, // owner
+      player[4].querySelector('div').innerText, // gamesplayed
+      player[5].querySelector('div').innerText, // points
+      player[9].querySelector('div').innerText, // pass yards
+      player[10].querySelector('div').innerText, // pass td's
+      player[11].querySelector('div').innerText, // interceptions
+      player[12].querySelector('div').innerText, // rush attempts
+      player[13].querySelector('div').innerText, // rush yards
+      player[14].querySelector('div').innerText, // rush td's
+      player[15].querySelector('div').innerText, // targets
+      player[16].querySelector('div').innerText, // receptions
+      player[17].querySelector('div').innerText, // reception yards
+      player[18].querySelector('div').innerText, // reception td's
+      player[19].querySelector('div').innerText, // return yards
+      player[20].querySelector('div').innerText, // return td's
+      player[21].querySelector('div').innerText, // 2pt conversions
+      player[22].querySelector('div').innerText // fumbles lost
+    ];
+
+    if (parsedPlayer[3] === '0.00') {
+      return true;
+    }
+
+    parsedPlayers.push(parsedPlayer.join('\t'));
+  }
+
+  return false;
+}
