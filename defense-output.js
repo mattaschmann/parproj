8b969819cad062b59b37d9f17403bd142fa06596
Matt Aschmann <matt.aschmann@gmail.com>
2015-10-21 14:51:26 -0600

    add missed code change

--- defense-output.js
+++ defense-output.js
@@ -49,20 +49,6 @@ function scrapePlayers() {
       owner, // owner
       player[4].querySelector('div').innerText, // gamesplayed
       player[5].querySelector('div').innerText, // points
-      player[9].querySelector('div').innerText, // pass yards
-      player[10].querySelector('div').innerText, // pass td's
-      player[11].querySelector('div').innerText, // interceptions
-      player[12].querySelector('div').innerText, // rush attempts
-      player[13].querySelector('div').innerText, // rush yards
-      player[14].querySelector('div').innerText, // rush td's
-      player[15].querySelector('div').innerText, // targets
-      player[16].querySelector('div').innerText, // receptions
-      player[17].querySelector('div').innerText, // reception yards
-      player[18].querySelector('div').innerText, // reception td's
-      player[19].querySelector('div').innerText, // return yards
-      player[20].querySelector('div').innerText, // return td's
-      player[21].querySelector('div').innerText, // 2pt conversions
-      player[22].querySelector('div').innerText // fumbles lost
     ];
 
     if (parsedPlayer[5] === '0.00') {
