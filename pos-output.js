d92267efeddff68c2d2e41c2fa4f2b2e38efdfcf
Matt Aschmann <matt.aschmann@gmail.com>
2017-11-29 10:51:10 -0700

    Fix off by one caused by forecast

--- pos-output.js
+++ pos-output.js
@@ -39,25 +39,25 @@ function scrapePlayers() {
       player[1].querySelector('.name').innerText, // player name
       player[1].querySelector('.ysf-player-name span').innerText, // position
       opponent, // opponent
-      player[3].querySelector('div').innerText,  // owner
-      player[4].querySelector('div').innerText,  // gamesplayed
-      player[6].querySelector('div').innerText,  // points
-      player[10].querySelector('div').innerText, // pass yards
-      player[11].querySelector('div').innerText, // pass td's
-      player[12].querySelector('div').innerText, // interceptions
-      player[13].querySelector('div').innerText, // rush attempts
-      player[14].querySelector('div').innerText, // rush yards
-      player[15].querySelector('div').innerText, // rush td's
-      player[16].querySelector('div').innerText, // targets
-      player[17].querySelector('div').innerText, // receptions
-      player[18].querySelector('div').innerText, // reception yards
-      player[19].querySelector('div').innerText, // reception td's
-      player[20].querySelector('div').innerText, // return yards
-      player[21].querySelector('div').innerText, // return td's
-      player[22].querySelector('div').innerText, // 2pt conversions
-      player[23].querySelector('div').innerText  // fumbles lost
+      player[4].querySelector('div').innerText,  // owner
+      player[5].querySelector('div').innerText,  // gamesplayed
+      player[7].querySelector('div').innerText,  // points
+      player[11].querySelector('div').innerText, // pass yards
+      player[12].querySelector('div').innerText, // pass td's
+      player[13].querySelector('div').innerText, // interceptions
+      player[14].querySelector('div').innerText, // rush attempts
+      player[15].querySelector('div').innerText, // rush yards
+      player[16].querySelector('div').innerText, // rush td's
+      player[17].querySelector('div').innerText, // targets
+      player[18].querySelector('div').innerText, // receptions
+      player[19].querySelector('div').innerText, // reception yards
+      player[20].querySelector('div').innerText, // reception td's
+      player[21].querySelector('div').innerText, // return yards
+      player[22].querySelector('div').innerText, // return td's
+      player[23].querySelector('div').innerText, // 2pt conversions
+      player[24].querySelector('div').innerText  // fumbles lost
     ];
-
+    
     if (parsedPlayer[5] === '0.00') {
       return true;
     }
